0412da889555cb85a93d62d3837932d2bfe6b04f
amirbh61 <amirbh61@gmail.com>
2023-06-26 20:28:07 +0300

    Sorted square fiber macros for local and cluster to properly generate all macro files

--- square_fiber_generate_macros_Cluster.py
+++ square_fiber_generate_macros_Cluster.py
@@ -12,7 +12,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 plt.style.use('classic')
-#from tqdm import tqdm
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', 500)
 import re
@@ -22,14 +21,24 @@ import itertools
 # In[0]
 # generate new geant4 macros for all geometries and all possible runs
 
-# Geometry parameters
+# # Geometry parameters
+# geometry_params = {
+#     'ELGap': ['1', '10'],
+#     'pitch': ['5', '10', '15.6'],
+#     'distanceFiberHolder': ['-1', '2', '5'],
+#     'distanceAnodeHolder': ['2.5', '5', '10'],
+#     'holderThickness': ['5','10'],
+#     'TPBThickness': ['2.2'] # microns
+# }
+
+# Geometry parameters - CLUSTER TEST DELETE LATER
 geometry_params = {
-    'ELGap': ['1', '10'],
-    'pitch': ['5', '10', '15.6'],
-    'distanceFiberHolder': ['-1', '2', '5'],
-    'distanceAnodeHolder': ['2.5', '5', '10'],
-    'holderThickness': ['5','10'],
-    'TPBThickness': ['2.2'] # microns
+    'ELGap': ['10'],
+    'pitch': ['5'],
+    'distanceFiberHolder': ['2'],
+    'distanceAnodeHolder': ['2.5', '10'],
+    'holderThickness': ['10'],
+    'TPBThickness': ['2.2']  # microns
 }
 
 # Run parameters
@@ -39,8 +48,7 @@ run_params = {
     'z': ['0'],
 }
 
-unit_cell_source_spacing = 0.5 # mm, spacing between sources in different runs
-z = 0 # junk number, value has no meaning but must exist as input for geant
+unit_cell_source_spacing = 0.5  # mm, spacing between sources in different runs
 seed = 10000
 
 original_macro_path = r'/gpfs0/arazi/users/amirbenh/Resolving_Power/nexus/macros/SquareOpticalFiberCluster.config.mac'
@@ -48,7 +56,7 @@ output_macro_Mfolder = r'/gpfs0/arazi/users/amirbenh/Resolving_Power/nexus/Squar
 
 if not os.path.isdir(output_macro_Mfolder):
     os.mkdir(output_macro_Mfolder)
-    
+
 # Open your macro file and read in the data
 with open(original_macro_path, "r") as f:
     template = f.read()
@@ -56,57 +64,61 @@ with open(original_macro_path, "r") as f:
 # Get the Cartesian product of all parameter values
 geometry_combinations = list(itertools.product(*geometry_params.values()))
 
-#for i, combination in tqdm(enumerate(geometry_combinations)):
+# Iterate through each geometry combination
 for i, combination in enumerate(geometry_combinations):
-    
-    output_macro_geom_folder = output_macro_Mfolder + \
-                                f'ELGap={combination[0].replace(" ","")}mm_' + \
-                                f'pitch={combination[1].replace(" ","")}mm_' + \
-                                f'distanceFiberHolder={combination[2].replace(" ","")}mm_' + \
-                                f'distanceAnodeHolder={combination[3].replace(" ","")}mm_' + \
-                                f'holderThickness={combination[4].replace(" ","")}mm'
-                                
+
+    # Generate output directory based on geometry
+    output_macro_geom_folder = os.path.join(output_macro_Mfolder,
+                                            f'ELGap={combination[0]}mm_'
+                                            f'pitch={combination[1]}mm_'
+                                            f'distanceFiberHolder={combination[2]}mm_'
+                                            f'distanceAnodeHolder={combination[3]}mm_'
+                                            f'holderThickness={combination[4]}mm')
+
+    # Create directory if it doesn't exist
     if not os.path.isdir(output_macro_geom_folder):
         os.mkdir(output_macro_geom_folder)
-        
-    # Fill x,y,seed values in macros for each geometry setup
+
+    # Update x and y values in run_params for the 'pitch' parameter
+    for key, value in zip(geometry_params.keys(), combination):
+        if key == 'pitch':
+            pitch_value = float(value)
+            x = np.arange(-pitch_value / 2, pitch_value / 2 + unit_cell_source_spacing, unit_cell_source_spacing)
+            y = np.arange(-pitch_value / 2, pitch_value / 2 + unit_cell_source_spacing, unit_cell_source_spacing)
+            
+            # Update the run parameters
+            run_params = {
+                'x': [str(val) for val in x],
+                'y': [str(val) for val in y],
+            }
+            break
+
+    # Iterate through each combination of x and y
     for x_val in run_params['x']:
         for y_val in run_params['y']:
-            x_val = str(round(float(x_val),3))
-            y_val = str(round(float(y_val),3))
             
-            macro = template # Define a fresh copy of the macro template
+            # Start with a fresh copy of the macro template
+            macro = template
+            
+            # Replace geometry parameters in the macro
             for key, value in zip(geometry_params.keys(), combination):
-                if key == 'pitch':
-                    pitch_value = float(value.split(' ')[0])
-                    x = np.arange(-pitch_value/2, pitch_value/2 + unit_cell_source_spacing,
-                                  unit_cell_source_spacing )
-                    y = np.arange(-pitch_value/2, pitch_value/2 + unit_cell_source_spacing,
-                                  unit_cell_source_spacing )
-                    
-                    # Update the run parameters
-                    run_params = {
-                        'x': [str(val) for val in x],
-                        'y': [str(val) for val in y],
-                    }
                 macro = macro.replace('${' + key + '}', value)
                 
+            # Replace x, y and seed in the macro
             macro = macro.replace('${x}', x_val)
             macro = macro.replace('${y}', y_val)
             macro = macro.replace('${seed}', str(seed))
             seed += 1
-            
-            output_SiPM_path = output_macro_geom_folder + \
-                f'/SiPM_hits_x={x_val}mm_y={y_val}mm.txt'                     
+
+            # Output paths
+            output_SiPM_path = os.path.join(output_macro_geom_folder, f'SiPM_hits_x={x_val}mm_y={y_val}mm.txt')
             macro = macro.replace('${sipmOutputFile_}', output_SiPM_path)
             
-            output_TPB_path = output_macro_geom_folder + \
-                f'/TPB_hits_x={x_val}mm_y={y_val}mm.txt'
+            output_TPB_path = os.path.join(output_macro_geom_folder, f'TPB_hits_x={x_val}mm_y={y_val}mm.txt')
             macro = macro.replace('${tpbOutputFile_}', output_TPB_path)
             
-            output_macro_path = output_macro_geom_folder + \
-            f'/x={x_val}mm_y={y_val}mm.config.mac'
-                                  
+            output_macro_path = os.path.join(output_macro_geom_folder, f'x={x_val}mm_y={y_val}mm.config.mac')
+            
             # Write the new macro to a new file
             with open(output_macro_path, "w") as f:
                 f.write(macro)
